6d2720cfebed168c68cc4af22a81e010eeb2235d
Mele <61437633+Melecio-Leyva@users.noreply.github.com>
2022-09-16 14:42:16 -0700

    Exception hanlding added for files not found.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 from random import choice, randint, shuffle
 from tkinter import messagebox
 import pyperclip
-
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -18,7 +18,7 @@ def generate_password():
     password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
     # append all lists together
     password_list = password_letters + password_symbols + password_numbers
-    #shuffle the list
+    # shuffle the list
     shuffle(password_list)
     # Use join to append all the calues of the shuffled list into a single string
     password = "".join(password_list)
@@ -32,19 +32,46 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = { website:{
+        "email":email,
+        "password": password
+    }}
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Ooops", message="Please dont leave any input fields empty.")
     else:
-        is_ok = messagebox.askokcancel(title=website,
-                                       message=f"These are the details entered \nEmail: {email} \nPassword: "
-                                               f"{password}")
-        if is_ok:
-            # append the password to the file
-            with open("data.txt", "a") as data_file:
-                data_file.write(f" {website} | {email} | {password} \n")
-                website_entry.delete(0, END)
-                password_entry.delete((0, END))
+        try:
+            # read old data from json
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
 
+        except FileNotFoundError:
+            # if not found make new json data
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # else update trhe data into the json file
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+# ---------------------------- FIND PASSWORD ------------------------------- #
+
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data found.")
+    else:
+        if website in data:
+            email = data[website]['email']
+            password = data[website]['password']
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"no details currently exist for the website {website}")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -65,15 +92,17 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=3)
-email_entry = Entry(width=35)
+website_entry = Entry(width=30)
+website_entry.grid(row=1, column=1)
+email_entry = Entry(width=30)
 email_entry.insert(0, "example@gmail.com")
-email_entry.grid(row=2, column=1, columnspan=2)
-password_entry = Entry(width=21)
+email_entry.grid(row=2, column=1)
+password_entry = Entry(width=30)
 password_entry.grid(row=3, column=1)
 
 # buttons
+search_button = Button(text="Search", width=14, command=find_password)
+search_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
